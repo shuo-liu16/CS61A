bf69212894a7d4530a212403b50842b25b4395ef
shuo_liu <shuo_liu16@outlook.com>
2024-10-21 17:25:39 +0800

    proj4 start

--- proj/scheme/scheme_classes.py
+++ proj/scheme/scheme_classes.py
@@ -26,15 +26,18 @@ class Frame:
     def define(self, symbol, value):
         """Define Scheme SYMBOL to have VALUE."""
         # BEGIN PROBLEM 1
-        "*** YOUR CODE HERE ***"
+        self.bindings[symbol] = value
         # END PROBLEM 1
 
     def lookup(self, symbol):
         """Return the value bound to SYMBOL. Errors if SYMBOL is not found."""
         # BEGIN PROBLEM 1
-        "*** YOUR CODE HERE ***"
-        # END PROBLEM 1
-        raise SchemeError('unknown identifier: {0}'.format(symbol))
+        if symbol in self.bindings:
+            return self.bindings[symbol]
+        elif self.parent is not None:
+            return self.parent.lookup(symbol)
+        else:
+            raise SchemeError('unknown identifier: {0}'.format(symbol))
 
 
     def make_child_frame(self, formals, vals):
